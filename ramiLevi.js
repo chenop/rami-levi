3260ce7640b1dbdb56807efbc26a494eeedf2556
Chen Oppenhaim <chen.oppenhaim@toluna.com>
2020-04-06 23:48:04 +0300

    first commit

---
+++ ramiLevi.js
@@ -0,0 +1,34 @@
+const axios = require('axios');
+const Pushover = require('node-pushover');
+require('dotenv').config(); // Read .env file
+
+const push = new Pushover({
+	token: process.env.APP_TOKEN,
+	user: process.env.USER_API_KEY
+});
+
+async function fetchAvailableHours() {
+	let result = await axios.post('https://api-prod.rami-levy.co.il/api/v1/orders/get-supply-date',
+		{
+			"city": "חיפה", "street": "הגליל", "street_number": "138"
+		}
+	);
+
+	let shifts = result.data.data;
+	let availableShifts = shifts.filter(day => day.Active === '1');
+	return availableShifts.map(shift => `${shift.Date} ${shift.FromHour}-${shift.ToHour}`);
+}
+
+const run = async () => {
+	let hours = await fetchAvailableHours();
+	if (!hours || hours.length === 0)
+		return;
+
+	let formattedHours = hours.join('\n');
+	console.log(formattedHours);
+
+	// We got available hours!
+	push.send("רמי לוי פנוי!", formattedHours);
+};
+
+run();
